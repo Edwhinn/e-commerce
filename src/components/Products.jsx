5955dafa3c84ba4f00964c5ed255acd429c2200b
Edwin <evill096@fiu.edu>
2022-09-22 04:51:03 -0400

    450 9/22/2022

---
+++ src/components/Products.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import {popularProducts} from '../data'
+
+const Container = styled.div`
+    padding: 20px;
+    display: flex;
+`
+
+const Products = () => {
+  return (
+    <Container>{popularProducts.map(item => (
+        <Products item={item} key={item.id}/>
+    ))}
+
+    </Container>
+  )
+}
+
+export default Products
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Announcement from '../components/Announcement'
 import { Categories } from '../components/Categories'
 import Navbar from '../components/Navbar'
+import Products from '../components/Products'
 import Slider from '../components/Slider'
 
 
@@ -14,7 +15,7 @@ const Home = () => {
     <Announcement/>
     <Slider/>
     <Categories/>
-
+    <Products/>
     </div>
   )
 }
